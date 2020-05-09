3406dbbb7e3cd08634dca3ef8018b41cd73769b0
Luis R Carrillo <luisr_carrillo@outlook.com>
2020-05-09 13:46:52 -0500

    Rutas, refactor de AppComponent y generacion de LoginComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { Graphics1Component } from './pages/graphics1/graphics1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     Graphics1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
