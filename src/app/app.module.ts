a5f29df3cbc027f9a18958994e5a0cda4216fc42
Luis R Carrillo <luisr_carrillo@outlook.com>
2020-06-15 21:38:32 -0500

    Configuraciones para librerias Sweetalert2 y Animate.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
@@ -8,10 +8,18 @@ import { LoginComponent } from './login/login.component';
 
 import { RegisterComponent } from './login/register.component';
 import { PagesModule } from './pages/pages.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [AppComponent, LoginComponent, RegisterComponent],
-    imports: [BrowserModule, PagesModule, FormsModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        PagesModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { RegisterComponent } from './login/register.component';
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-
     { path: '**', component: NopagefoundComponent },
 ];
 
