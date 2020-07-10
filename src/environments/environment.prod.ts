699de82dafb54788b45a44a6bb982bd649a7892a
Luis R Carrillo <luisr_carrillo@outlook.com>
2020-07-10 10:54:05 -0500

    Cambio de endpoint para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+    URL_SERVICIOS: 'https://api.luiscarrillo.mx',
+    GOOGLE_AUTH_API:
+        '620849696918-il4n41vis0auubhh85dbe1q2675b1172.apps.googleusercontent.com',
 };
